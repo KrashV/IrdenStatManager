ef32130584e6a8eec80bd247635d5256a5b95f22
mofuro <alexandr.xabarov@gmail.com>
2024-07-20 22:10:29 +0500

    Removed equipTech, ruler usage has been reworked to starExtensions. Works fine

--- scripts/irdenDrawables.lua
+++ scripts/irdenDrawables.lua
@@ -40,6 +40,11 @@ function init()
         player.interact("ScriptPane", sb.jsonMerge(config, data))
     end))
 
+    --message.setHandler("irdenStatManagerToShowMovement", localHandler(function(showLevel)
+    --    self.showLevel = showLevel
+    --    self.showPointer = false
+    --end))
+
     message.setHandler("irdenGetArmourMovement", localHandler(function(newMovement)
         self.movement = newMovement ~= 0 and newMovement or self.movement
     end))
@@ -49,10 +54,6 @@ function init()
         self.showPointer = false
     end))
 
-    message.setHandler("irdenStatManagerPointer", localHandler(function(pointerPosition)
-        self.showPointer = not self.showPointer
-        self.pointerPosition = pointerPosition
-    end))
 
     message.setHandler("ism_your_turn", simpleHandler(function(fightName)
         local conf = root.assetJson("/interface/scripted/irdenstatmanager/ismnotify/ismnotify.config")
@@ -74,110 +75,113 @@ function update(...)
         local distanceInBlocks = self.movement * self.movementMultiplier
         local stanartDistanceInBlocks = self.standartMovement * self.movementMultiplier
 
-for _, p in ipairs(world.playerQuery(mPosition, 50, { withoutEntityId = pId })) do
-    local entityPos = world.entityPosition(p)
-    local distance = vec2.sub(entityPos, mPosition)
-    local blockDistance = vec2.mag(vec2.sub(distance, mouthOffset)) / distanceInBlocks
-    local standartBlockDistance = vec2.mag(vec2.sub(distance, mouthOffset)) / stanartDistanceInBlocks
+        for _, p in ipairs(world.playerQuery(mPosition, 50, { withoutEntityId = pId })) do
+            local entityPos = world.entityPosition(p)
+            local distance = vec2.sub(entityPos, mPosition)
+            local blockDistance = vec2.mag(vec2.sub(distance, mouthOffset)) / distanceInBlocks
+            local standartBlockDistance = vec2.mag(vec2.sub(distance, mouthOffset)) / stanartDistanceInBlocks
 
-    local n = blockDistance < 0.3 and "X" or math.min(math.floor(blockDistance + 1), 9)
-    local v = standartBlockDistance < 0.3 and "X" or math.min(math.floor(standartBlockDistance + 1), 9)
+            local n = blockDistance < 0.3 and "X" or math.min(math.floor(blockDistance + 1), 9)
+            local v = standartBlockDistance < 0.3 and "X" or math.min(math.floor(standartBlockDistance + 1), 9)
 
-    local drawable1 = {
-        image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", n),
-        position = vec2.add(distance, { 0, 2 }),
-        color = self.numberColor,
-        fullbright = true
-    }
+            local drawable1 = {
+                image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", n),
+                position = vec2.add(distance, { 0, 2 }),
+                color = self.numberColor,
+                fullbright = true
+            }
 
-    localAnimator.addDrawable(drawable1, "overlay")
+            localAnimator.addDrawable(drawable1, "overlay")
 
-    if distanceInBlocks ~= stanartDistanceInBlocks then
-        local drawable2 = {
-            image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", v),
-            position = vec2.add(distance, { 0, 3.5 }),
-            color = self.numberColor,
-            fullbright = true
-        }
+            if distanceInBlocks ~= stanartDistanceInBlocks then
+                local drawable2 = {
+                    image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", v),
+                    position = vec2.add(distance, { 0, 3.5 }),
+                    color = self.numberColor,
+                    fullbright = true
+                }
 
-        local drawable3 = {
-            image = "/interface/scripted/irdenstatmanager/numbers/armored.png",
-            position = vec2.add(distance, { 1, 2 }),
-            color = self.numberColor,
-            fullbright = true
-        }
+                local drawable3 = {
+                    image = "/interface/scripted/irdenstatmanager/numbers/armored.png",
+                    position = vec2.add(distance, { 1, 2 }),
+                    color = self.numberColor,
+                    fullbright = true
+                }
 
-        localAnimator.addDrawable(drawable2, "overlay")
-        localAnimator.addDrawable(drawable3, "overlay")
-    end
-end
+                localAnimator.addDrawable(drawable2, "overlay")
+                localAnimator.addDrawable(drawable3, "overlay")
+            end
+        end
 
         if self.showLevel > 2 then
             localAnimator.clearDrawables()
             -- Show ruler
-            promises:add(world.sendEntityMessage(pId, "irdenStatManagerGetAimPosition"), function(currentAimPosition)
-                local difference = { 0, 0 }
+            local difference = { 0, 0 }
 
-                if self.showPointer then
-                    difference = vec2.sub(self.pointerPosition, mPosition)
+            if (input.mouseDown("MouseLeft")) then
+                self.showPointer = not self.showPointer
+                self.pointerPosition = player.aimPosition()
+            end
 
-                    localAnimator.addDrawable({
-                        image = "/interface/scripted/irdenstatmanager/numbers/arrow.png",
-                        position = difference,
-                        color = self.numberColor,
-                        fullbright = true
-                    }, "overlay")
-                else
-                    difference = vec2.sub(currentAimPosition, mPosition)
-                end
-
+            if self.showPointer then
+                difference = vec2.sub(self.pointerPosition, mPosition)
 
                 localAnimator.addDrawable({
-                    line = { mouthOffset, { difference[1], mouthOffset[2] } },
-                    position = { 0, 0 },
-                    color = self.lineColor,
-                    fullbright = true,
-                    width = 1
+                    image = "/interface/scripted/irdenstatmanager/numbers/arrow.png",
+                    position = difference,
+                    color = self.numberColor,
+                    fullbright = true
                 }, "overlay")
+            else
+                difference = vec2.sub(player.aimPosition(), mPosition)
+            end
+
+
+            localAnimator.addDrawable({
+                line = { mouthOffset, { difference[1], mouthOffset[2] } },
+                position = { 0, 0 },
+                color = self.lineColor,
+                fullbright = true,
+                width = 1
+            }, "overlay")
+
+            localAnimator.addDrawable({
+                line = { { difference[1], mouthOffset[2] }, difference },
+                position = { 0, 0 },
+                color = self.lineColor,
+                fullbright = true,
+                width = 1
+            }, "overlay")
+
+
+            local blockDistance = vec2.mag(vec2.sub(difference, mouthOffset)) / distanceInBlocks
+            local n = blockDistance < 0.3 and "X" or math.min(math.floor(blockDistance + 1), 9)
+
+            localAnimator.addDrawable({
+                image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", n),
+                position = { difference[1], mouthOffset[2] },
+                color = self.numberColor,
+                fullbright = true
+            }, "overlay")
+
+            if distanceInBlocks ~= stanartDistanceInBlocks then
+                local standartBlockDistance = vec2.mag(vec2.sub(difference, mouthOffset)) / stanartDistanceInBlocks
+                local v = blockDistance < 0.3 and "X" or math.min(math.floor(standartBlockDistance + 1), 9)
 
                 localAnimator.addDrawable({
-                    line = { { difference[1], mouthOffset[2] }, difference },
-                    position = { 0, 0 },
-                    color = self.lineColor,
-                    fullbright = true,
-                    width = 1
-                }, "overlay")
-
-
-                local blockDistance = vec2.mag(vec2.sub(difference, mouthOffset)) / distanceInBlocks
-                local n = blockDistance < 0.3 and "X" or math.min(math.floor(blockDistance + 1), 9)
-
-                localAnimator.addDrawable({
-                    image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", n),
-                    position = { difference[1], mouthOffset[2] },
+                    image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", v),
+                    position = { difference[1], mouthOffset[2] + 1.5 },
                     color = self.numberColor,
                     fullbright = true
                 }, "overlay")
 
-                if distanceInBlocks ~= stanartDistanceInBlocks then
-                    local standartBlockDistance = vec2.mag(vec2.sub(difference, mouthOffset)) / stanartDistanceInBlocks
-                    local v = blockDistance < 0.3 and "X" or math.min(math.floor(standartBlockDistance + 1), 9)
-
-                    localAnimator.addDrawable({
-                        image = string.format("/interface/scripted/irdenstatmanager/numbers/%s.png", v),
-                        position = { difference[1], mouthOffset[2] + 1.5 },
-                        color = self.numberColor,
-                        fullbright = true
-                    }, "overlay")
-
-                    localAnimator.addDrawable({
-                        image = "/interface/scripted/irdenstatmanager/numbers/armored.png",
-                        position = { difference[1] + 1.2, mouthOffset[2] },
-                        color = self.numberColor,
-                        fullbright = true
-                    }, "overlay")
-                end
-            end)
+                localAnimator.addDrawable({
+                    image = "/interface/scripted/irdenstatmanager/numbers/armored.png",
+                    position = { difference[1] + 1.2, mouthOffset[2] },
+                    color = self.numberColor,
+                    fullbright = true
+                }, "overlay")
+            end
         end
     end
 
